4faf8a1b3a18c0ebf6375fd9d023c8ddce6f1dbd
Emanuel Grigolatto <ema_grigo@hotmail.com>
2023-08-26 17:31:52 -0300

    segundo commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://egrigolatto.github.io/<REPO>/",
+  base:"/App_Rick_and_Morty/",
   plugins: [react()],
 })
